9228416919c970d9008f731dfc25c9cd838b846b
tyler <tyler@localhost.localdomain>
2017-08-14 15:56:42 -0400

    The finished product after Hack Day with all the test sets

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sentimentanalysis
+
+I built this for a Hackathon so please excuse the mess of code. I will get to cleaning it up soon. 
+
+This code does a LIVE Sentiment Analysis on incoming tweets about any subject you add to the filter. 
+
+Setup:
+1. Add tokens to top part of file from your Twitter dev account
+2. Change filter to whatever you want to comes from Twitter
+3. I have also commented out the ability to save your training model. I had some issues on Hack Day with the sentiment that came out of the saved model so I commented it out and just did training on the 2kTestSet.csv before the stream started.
+
+
+Changes Coming:
+1. Configuration file for tokens and filters
+2. Code clean up
--- Sentiment.py
+++ Sentiment.py
@@ -1,23 +1,26 @@
 '''
 Created on Aug 2, 2017
-
 @author: tyler reece
 '''
 
-import collections
 import json
 import re
-import operator
-import string
-import math
 import csv
 import nltk
-from collections import defaultdict
-from collections import Counter
-from nltk.corpus import stopwords
-from textblob import TextBlob
-from textblob.sentiments import NaiveBayesAnalyzer
-from nltk.tbl import feature
+import tweepy
+import time
+import pickle
+import codecs
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from datetime import datetime
+
+# keys and tokens from the Twitter Dev Console
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 #start replaceTwoOrMore
 def replaceTwoOrMore(s):
@@ -26,7 +29,6 @@ def replaceTwoOrMore(s):
     return pattern.sub(r"\1\1", s)
 #end
  
-#start getStopWordList
 def getStopWordList(stopWordListFileName):
     #read the stopwords file and build a list
     stopWords = []
@@ -40,9 +42,9 @@ def getStopWordList(stopWordListFileName):
             stopWords.append(word)
             line = stopList.readline()
     return stopWords
-#end
 
-#start getfeatureVector
+stopWords = getStopWordList('stopwords.txt')
+
 def getFeatureVector(tweet):
     featureVector = []
     #split tweet into words
@@ -60,11 +62,8 @@ def getFeatureVector(tweet):
         else:
             featureVector.append(w.lower())
     return featureVector
-#end
 
-def preprocess(tweet):
-    # process the tweets
-
+def processTweet(tweet):
     #Convert to lower case
     tweet = tweet.lower()
     #Convert www.* or https?://* to URL
@@ -79,64 +78,168 @@ def preprocess(tweet):
     tweet = tweet.strip('\'"')
     return tweet
 
-#start extract_features
 def extract_features(tweet):
     tweet_words = set(tweet)
     features = {}
-    print featureList
     for word in featureList:
         features['contains(%s)' % word] = (word in tweet_words)
-    print features
     return features
-#end
 
-stopWords = getStopWordList('stopwords.txt')
 featureList = []
 tweets = []
 
-with open('positive_words.txt', 'r') as positiveFile:
-    positiveSentiment = 'positive'
-    #positiveFeatureVector = positiveFile.read().split(',')
-    positiveWords = 'good,nice,cool,yummy'
-    positiveFeatureVector = positiveWords.split(',')
-    featureList.extend(positiveFeatureVector)
-    tweets.append((positiveFeatureVector, positiveSentiment));
+print ('Training classfier.....')
+print str(datetime.now())
 
-with open('negative_words.txt', 'r') as negativeFile:
-    neutralSentiment = 'neutral'
-    #negativeFeatureVector = negativeFile.read().split(',')
-    neutralWords = 'okay,sure,wtf'
-    neutralFeatureVector = neutralWords.split(',')
-    featureList.extend(neutralFeatureVector)
-    tweets.append((neutralFeatureVector, neutralSentiment));
+inpTweets = csv.reader(open('2kTestSet.csv', 'rU'), delimiter=',', quotechar='|')
 
-with open('negative_words.txt', 'r') as negativeFile:
-    negativeSentiment = 'negative'
-    #negativeFeatureVector = negativeFile.read().split(',')
-    negativeWords = 'bad,mean,nasty'
-    negativeFeatureVector = negativeWords.split(',')
-    featureList.extend(negativeFeatureVector)
-    tweets.append((negativeFeatureVector, negativeSentiment));
+for row in inpTweets:
+    sentiment = row[0];
+    tweet = row[1]
+    processedTweet = processTweet(tweet)
+    featureVector = getFeatureVector(processedTweet)
+    featureList.extend(featureVector)
+    tweets.append((featureVector, sentiment));
 
-print tweets     
 # Extract feature vector for all tweets in one shot
 training_set = nltk.classify.util.apply_features(extract_features, tweets)
-
 # Train the classifier
 NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
+#f = open('my_classifier_20k.pickle', 'wb')
+#pickle.dump(NBClassifier, f)
+#f.close()
 
-# Test the classifier
-testTweet = 'good nice cool happy'
-processedTestTweet = preprocess(testTweet) 
+#print ('classifier stored as my_classifier.pickle')
+#f = open('my_classifier_10k.pickle', 'rb')
+#NBClassifier = pickle.load(f)
+#f.close()
 
-#print processedTestTweet
-#print extract_features(getFeatureVector(processedTestTweet))
+print ('Classifier loaded. Now classifying tweets.')
+print str(datetime.now())
 
+#Twitter streaming start ------------------------------------------------------------------------
+# attempt authentication
+try:
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+     
+    api = tweepy.API(auth)
+except:
+    print("Error: Authentication Failed")
+ 
+class MyListener(StreamListener):
+    
+    def __init__(self, time_limit=600):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.saveFile = open('TweetSentiment.csv', 'a')
+        super(StreamListener, self).__init__()
+    
+    def on_connect(self):
+        print('Connected to Stream')
+        pass
+ 
+    def on_data(self, data):
+        try:
+            tweet = json.loads(data)
+            if (time.time() - self.start_time) < self.limit:
+                if 'text' in tweet:
+                    processedTweet = processTweet(tweet['text']) 
+                    tweetSentiment = NBClassifier.classify(extract_features(getFeatureVector(processedTweet)))
+                    print tweetSentiment
+                else:
+                    processedTweet = 'no tweet'
+                    tweetSentiment = 'none'
+                if 'id_str' in tweet:
+                        idStr = tweet['id_str']
+                else:
+                     idStr = 'none'
+                        
+                if 'created_at' in tweet:
+                    createdAt = tweet['created_at']
+                else:
+                     createdAt = 'none'
+                        
+                if 'favorite_count' in tweet:
+                    favoriteCount = tweet['favorite_count']
+                else:
+                    favoriteCount = 'none'
+                        
+                if 'retweeted_count' in tweet:
+                    retweetedCount = tweet['retweeted_count']
+                else:
+                    retweetedCount = 'none'
+                        
+                if 'user' in tweet:
+                    user = tweet['user']
+                    if 'screen_name' in user:
+                        screenName = user['screen_name']
+                    else:
+                        screenName = 'none'
+                    if 'statuses_count' in user:
+                        statusesCount = user['statuses_count']
+                    else:
+                        statusesCount = 'none'
+                    if 'followers_count' in user:
+                        followersCount = user['followers_count']
+                    else:
+                        followersCount = 'none'
+                    if 'location' in user:
+                        location = user['location']
+                    else:
+                        location = 'none'
+                    if 'lang' in user:
+                        language = user['lang']
+                    else:
+                        language = 'none'
+                else:
+                    user = 'none'
+                
+                0pP1SpJqHIOG282pOFtLySVSVA8OReqM0Aqj4nZPLOvAH
+                self.saveFile.write(tweetSentiment)
+                self.saveFile.write('|')
+                self.saveFile.write(str(idStr).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(processedTweet.encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(screenName).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(createdAt).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(favoriteCount).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(retweetedCount).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(statusesCount).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(followersCount).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(location).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write(str(language).encode("utf-8"))
+                self.saveFile.write('|')
+                self.saveFile.write('\n')
+                return True
+            else:
+                self.saveFile.close()
+                print('Stream complete!')
+                return False
+
+        except BaseException as e:
+                print("Error on_data: %s" % str(e))        
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+ #change these to stream whatever filter you want
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#zeke' , '#zekeelliott', '#EzekielElliott', '#cowboys', '#NFL', '#dallascowboys', '#football'])
+
+#Twitter streaming end ------------------------------------------------------------------------
+            
+print 'Sentiment complete'
+print str(datetime.now())
 #print extract_features(getFeatureVector(testTweet))  
-print NBClassifier.classify(extract_features(processedTestTweet))
-#print 'Features'
-#print features
-#print 'feature list' 
-#3print featureList
-#print 'tweets'
-#print tweets
+#print NBClassifier.classify(extract_features(getFeatureVector(processedTestTweet)))
